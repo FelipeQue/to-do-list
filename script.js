364066dde76490c2fac271b2a8a9a29a384080db
Felipe Quérette <felipe.querette@gmail.com>
2024-03-10 13:47:01 -0300

    Created functions getTaslList and createTask

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let taskList = document.getElementById("actual-list");
 let insertTask = document.getElementById("insert-task");
 let taskAmount;
 
+// O clique do botão adicionar:
+addTaskButton.addEventListener('click', function() {
+    // Verificar se existe algo escrito (aproveitando que Javascript é truthy):
+    if (insertTask.value) {
+        createTask(insertTask.value);
+        updateTaskAmount();
+        insertTask.value = "";   
+        renderList();     
+    };
+});
+
 // Eu quero que o enter também provoque o clique do botão adicionar:
 insertTask.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
@@ -12,56 +23,63 @@ insertTask.addEventListener("keypress", function(event) {
   }
 });
 
-// O clique do botão adicionar:
-addTaskButton.addEventListener('click', function() {
-    // console.log("Botão foi clicado e chamou a função.")
-    let newTask = document.getElementById("insert-task");
-    // Verificar se existe algo escrito (aproveitando que Javascript é truthy):
-    if (newTask.value) {
-        // console.log("Verificação funciona.")
-        // Criar novo elemento na lista:
-        let newItem = document.createElement("li");
-        newItem.classList.add("todo-item");
-        newItem.innerHTML = 
-        `<input type="checkbox" class="check-task"/>
-        <span class="todo-item-text">`
-        + newTask.value +
-        `</span> <button class="delete-button" title="Apagar tarefa"><i class='bx bxs-x-square'></i></button>`;
-      
-        // Aplicar a funcionalidade de remover tarefa no botão do item da lista que acabamos de criar:
-        let deleteButton = newItem.querySelector(".delete-button");
-        deleteButton.addEventListener("click", () => {
-          // console.log("Botão de deletar está sendo clicado.")
-          if (confirm("Tem certeza que deseja apagar esta tarefa?") == true) {
-            newItem.remove();
-            updateTaskAmount()
-          };
-        });
-        taskList.appendChild(newItem);
-        updateTaskAmount();
-        newTask.value = "";        
-    };
-});
+// Função para carregar a lista de tarefas do LocalStorage:
+function getTaskList(){
+  let taskListStorage = localStorage.getItem("tasks");
+  if(!!taskListStorage){
+    return JSON.parse(taskListStorage);
+  }
+  return [];
+};
 
-// Aplicar a funcionalidade do botão de remover uma tarefa para os itens que já existem na página inicialmente:
-// Instanciar todos os botões de apagar que já existem na página inicialmente e acrescentar um event listener para cada botão:
-let deleteButtons = document.querySelectorAll(".delete-button");
-deleteButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    // Encontrar o elemento mãe <li>:
-    let todoItem = button.closest(".todo-item");
-    if (todoItem) {
-      if (confirm("Tem certeza que deseja apagar esta tarefa?") == true) {
-        todoItem.remove();
-        updateTaskAmount();
-      };
-    };
-  });
-});
+// Função para criar objeto tarefa e adicionar ao LocalStorage:
+function createTask(taskName){
+  let task = {
+    name: taskName,
+    done: false
+  };
+  let taskListStorage = getTaskList();
+  taskListStorage.push(task);
+  localStorage.setItem("tasks", JSON.stringify(taskListStorage));
+  renderList();
+};
 
+// Função para "renderizar" as tarefas na página:
+function renderList(){
+  let taskListStorage = getTaskList()
+  taskList.innerHTML = "";
+
+  taskListStorage.map((task, index) => {
+    let id = 'tarefa-' + index;
+    let idButton = 'remover-tarefa-' + index;
+
+    let checkbox = document.createElement("input");
+    checkbox.classList.add("check-task");
+    checkbox.type = "checkbox";
+    checkbox.id = id;
+
+    let li = document.createElement("li");
+    li.classList.add("todo-item");
+    li.appendChild(checkbox);
+    li.innerHTML += `<span class="todo-item-text">`+ task.name + `</span>`;
+    li.innerHTML += `<button id='${idButton}' title="Apagar tarefa" class="delete-button"><i class='bx bxs-x-square'></i></button> `
+
+    taskList.appendChild(li)
+
+    let idCheckbox = document.getElementById(id);
+    idCheckbox.checked = task.done;
+  })
+};
+
+// Função para remover uma tarefa do LocalStorage:
+// EXERCICIO 03
+
+// Função para marcar uma tarefa como concluída:
+// EXERCICIO 02
+
+// Função que mostra a quantidade de tarefas:
 function updateTaskAmount() {
-  let todoItems = document.getElementsByClassName("todo-item");
-  taskAmount = todoItems.length;
+  taskAmount = getTaskList().length;
   if (taskAmount == 0) {
     let emptyMessage = document.createElement("p");
     emptyMessage.innerText = "Você não tem nenhuma tarefa."
@@ -77,5 +95,8 @@ function updateTaskAmount() {
   taskAmountCounter.innerText = taskAmount;
 }
 
-// Chamamos a função também aqui para que a página já carregue com um valor inicial referente aos items que estão no HTML:
+// Chamamos a função que "renderiza" as tarefas para que a página já carregue com as tarefas que estão no Local Storage:
+renderList()
+
+// Chamamos a função que mostra a quantidade de tarefas para que a página já carregue com um valor inicial referente aos items que estão no HTML:
 updateTaskAmount()
